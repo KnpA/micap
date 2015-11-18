22d02aaea54793edc2a60ec3b45d33530f333875
Morgan Bouculat <morgan.bouculat@gmail.com>
2015-11-18 09:15:19 +0100

    Grosse maj maggle

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+    qui = "Qui ?";
+    ou = "O&uacute ?";
+    quand = "Quand ?";
+    quoi = "Quoi ?"
+    
     wcircleparams = {
 		width: '65px',
 		height: '65px',
@@ -27,35 +32,47 @@ $(document).ready(function(){
         iconRotation:0
 	};
     
+    updatePhrase();
+    
 	$('.menu_qui').WCircleMenu(wcircleparams);
 	$('.menu_ou').WCircleMenu(wcircleparams);
 	$('.menu_quand').WCircleMenu(wcircleparams);
-    
     $('.menu_iot').WCircleMenu(wcircleparams_iot);
 
 	$('.menu_qui .wcircle-menu-item').on('click',function(){
         if(!$('.menu_qui').hasClass("wcircle-animating")) {
             $('.menu_qui .wcircle-icon').html($(this).html());
-            $('.menu_qui .wcircle-icon div').html($('.menu_qui .wcircle-icon img').attr('title'));
+            qui = $('.menu_qui .wcircle-icon img').attr('title');
+            $('.menu_qui .wcircle-icon div').html(qui);
+            updatePhrase();
        }
 	});
     $('.menu_ou .wcircle-menu-item').on('click',function(){
         if(!$('.menu_ou').hasClass("wcircle-animating")){
             $('.menu_ou .wcircle-icon').html($(this).html());
-            $('.menu_ou .wcircle-icon div').html($('.menu_ou .wcircle-icon img').attr('title'));
+            ou = $('.menu_ou .wcircle-icon img').attr('title');
+            $('.menu_ou .wcircle-icon div').html(ou);
+            updatePhrase();
         }
 	});
     $('.menu_quand .wcircle-menu-item').on('click',function(){
         if(!$('.menu_quand').hasClass("wcircle-animating")){           
             $('.menu_quand .wcircle-icon').html($(this).html());
-            $('.menu_quand .wcircle-icon div').html($('.menu_quand .wcircle-icon img').attr('title'));
+            quand = $('.menu_quand .wcircle-icon img').attr('title');
+            $('.menu_quand .wcircle-icon div').html(quand);
+            updatePhrase();
         }
 	});
     $('.menu_iot .wcircle-menu-item').on('click',function(){
         if(!$('.menu_iot').hasClass("wcircle-animating")){           
-		  $('.menu_iot .wcircle-icon').html($(this).html());
-       }
+            $('.menu_iot .wcircle-icon').html($(this).html());
+            quoi = $('.menu_iot .wcircle-icon img').attr('title');
+            $('.menu_iot .wcircle-icon div').html(quoi);
+            updatePhrase();
+        }
 	});
-
-
+    
+    function updatePhrase() {
+        $('.phrase').html("Si <span class='cyan'>"+qui+"</span> se trouve dans <span class='green'>"+ou+"</span> <span class='yellow'>"+quand+"</span> alors <span class='red'>"+quoi+"</span>.")
+    }
 });
